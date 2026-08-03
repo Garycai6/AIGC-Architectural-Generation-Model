aa1780d682bba36989f8f3115d7ea7efabeb9ea2
Garycai6 <caijiawei0528@gmail.com>
2026-08-04 01:12:30 +0800

    feat: 添加 BuildingParams 参数模型与校验

---
+++ generation/params/__init__.py
@@ -0,0 +1,19 @@
+from generation.params.model import (
+    STYLE_NAMES,
+    BuildingParams,
+    Environment,
+    Material,
+    Roof,
+    Style,
+    ViewAngle,
+)
+
+__all__ = [
+    "STYLE_NAMES",
+    "BuildingParams",
+    "Environment",
+    "Material",
+    "Roof",
+    "Style",
+    "ViewAngle",
+]
